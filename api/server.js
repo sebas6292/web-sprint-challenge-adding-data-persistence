62e605d7e709cb7a302166cf23157ad099f2fbd2
Sebastian <sebas6292@gmail.com>
2021-11-04 17:32:33 -0700

    port is listening on 5000

--- api/server.js
+++ api/server.js
@@ -1,1 +1,13 @@
-// build your server here and require it from index.js
+const express = require('express')
+// const recipesRouter = require('./recipes/recipes-router')
+const server = express()
+
+server.use(express.json())
+
+// server.use('/api/recipes', recipesRouter)
+
+server.use('*', (req, res) => {
+    res.json({ api: 'up' })
+})
+
+module.exports = server; 
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// start your server here
+require('dotenv').config()
+
+ const server = require("./api/server.js")
+
+ const port = process.env.PORT
+
+ server.listen(port, () => {
+     console.log(`API running on port ${port}`)
+ })
+
