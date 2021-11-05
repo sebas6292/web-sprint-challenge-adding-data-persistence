460572bb6b682a1947b1e49815485eb0bc77daff
Sebastian <sebas6292@gmail.com>
2021-11-04 17:55:33 -0700

    routers are set up but not fleshed out and nodemon is happy

--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,20 @@
-// build your `/api/projects` router here
+const router = require('express').Router()
+// const Recipe = require('./recipes-model')
+
+router.get('/', (req, res, next) => {
+        
+})
+
+router.post('/', (req, res, next) => {
+        
+})
+
+router.use((err, req, res, next) => { //eslint-disable-line
+    res.status(500).json({
+        customMessage: 'something went wrong inside project router',
+        message: err.message, 
+        stack: err.stack,
+    })
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,10 +1,16 @@
 const express = require('express')
-// const recipesRouter = require('./recipes/recipes-router')
+
+const projectRouter = require('./project/router')
+const resourceRouter = require('./resource/router')
+const taskRouter = require('./task/router')
+
 const server = express()
 
 server.use(express.json())
 
-// server.use('/api/recipes', recipesRouter)
+server.use('/api/project', projectRouter)
+server.use('/api/resource', resourceRouter)
+server.use('/api/task', taskRouter)
 
 server.use('*', (req, res) => {
     res.json({ api: 'up' })
